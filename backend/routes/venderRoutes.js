514c0c6e1cece42de28dd01de5ebef9c27e5c41a
Ahmad Ali <ahmadali.ashfaq786@gmail.com>
2024-05-02 09:06:10 +0500

    feat - venderService routes complete

--- backend/routes/venderRoutes.js
+++ backend/routes/venderRoutes.js
@@ -1,5 +1,13 @@
-const express = require('express')
+const express = require('express');
+// const { getOneService } = require('../controllers/venderController');
 const router = express.router(); 
 
+const {getOneService, createService,  deleteService, updateService, getServices} = require('../controllers/venderController')
 
+router.get('/', getServices); 
+router.get('/:id', getOneService); 
+router.post("/", createService); 
+router.delete('/:id',deleteService); 
+router.patch('/:id', updateService); 
 
+module.exports = router; 
