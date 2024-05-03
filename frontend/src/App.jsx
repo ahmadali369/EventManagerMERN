35f6a318f7f350c1b0792e8a31cb3fb845a48bf3
Ahmad Ali <ahmadali.ashfaq786@gmail.com>
2024-05-03 18:52:47 +0500

    feat - NavBar, Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 // import { useState } from 'react'
 
 import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 // import {Box} from '@mui/material'
 import ServiceForm from './componets/serviceForm'
+import NavBar from './componets/NavBar';
 
 function App() {
 
@@ -14,7 +16,25 @@ function App() {
 
 
 
-      <ServiceForm/>
+
+    <div className="App">
+      <BrowserRouter>
+        {/* <Navbar /> */}
+        <NavBar />
+        <div className="pages">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home />} 
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+
+
+
+      {/* <ServiceForm/> */}
 
 
         {/* <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
